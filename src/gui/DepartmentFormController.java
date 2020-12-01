c7378c1905a7e1b5ed274bcf9b185c96eddf8f51
leandrobrotas <leandromacedo.tecnologia@gmail.com>
2020-12-01 17:17:42 -0300

    Observer design pattern to update tableview

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
@@ -18,6 +21,12 @@ import model.entities.Department;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
+
+	private Department entity;
+
+	private DepartmentService service;
+
+	private List<DataChangeListener> dataChangeListenes = new ArrayList<>();
 
 	@FXML
 	private TextField txtId;
@@ -34,9 +43,17 @@ public class DepartmentFormController implements Initializable {
 	@FXML
 	private Button btCancel;
 
-	private Department entity;
+	public void setDeparment(Department entity) {
+		this.entity = entity;
+	}
 
-	private DepartmentService service;
+	public void setDeparmentService(DepartmentService service) {
+		this.service = service;
+	}
+
+	public void subscribeDataChangeListeners(DataChangeListener listener) {
+		dataChangeListenes.add(listener);
+	}
 
 	@FXML
 	public void onBtSaveAction(ActionEvent event) {
@@ -50,9 +67,16 @@ public class DepartmentFormController implements Initializable {
 		try {
 			entity = getFormData();
 			service.SaveOrUpdate(entity);
+			notifyDataChangeListeners();
 			Utils.currentStage(event).close();
 		} catch (DbException e) {
 			Alerts.showAlert("Error saving object!", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+
+	private void notifyDataChangeListeners() {
+		for (DataChangeListener listener : dataChangeListenes) {
+			listener.onDataChanged();
 		}
 	}
 
@@ -68,14 +92,6 @@ public class DepartmentFormController implements Initializable {
 	@FXML
 	public void onBtCancelAction(ActionEvent event) {
 		Utils.currentStage(event).close();
-	}
-
-	public void setDeparment(Department entity) {
-		this.entity = entity;
-	}
-
-	public void setDeparmentService(DepartmentService service) {
-		this.service = service;
 	}
 
 	@Override
